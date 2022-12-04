c0bfa5dd3818583dc79b2ece32a1b427e9081d8b
VikramTechnical <vikram.aptara@gmail.comm>
2022-12-04 22:16:36 +0530

    first commit

---
+++ js/script-vk.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // Plan details steps
+  $(".mobile-menu-btn").click(function () {
+    $(this).toggleClass("active");
+    $(".top-menu").toggleClass("active");
+    
+  });
+     
+
+
+  //
+});
