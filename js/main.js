2aceb7ac78cf1642bdff81692f5ab9859512b397
VikramTechnical <vikram.aptara@gmail.comm>
2022-12-27 00:07:47 +0530

    new version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
  
-$diy = $('.number-diy .data');
+$diy = $('.data');
 $diy.rollNumber({
+  number: $diy[0].dataset.number, 
+  speed: 600, 
+  interval: 200,
+  rooms: 5,
+  space: 40,
+  symbol: '', 
+  fontStyle: {
+    'font-size': 32,
+    'line-height': 40,
+  }
+})
+$diy1 = $('.data1');
+$diy1.rollNumber({
   number: $diy[0].dataset.number, 
   speed: 600, 
   interval: 200,
